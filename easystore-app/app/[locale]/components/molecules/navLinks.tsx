216b945d39f49671b44d7c6a3fb1217f43424750
hadelacruz <humbertoalexanderdelacruz@gmail.com>
2025-04-27 23:08:57 -0600

    feat: :sparkles: correct language display issue and finish header

--- easystore-app/app/[locale]/components/molecules/navLinks.tsx
+++ easystore-app/app/[locale]/components/molecules/navLinks.tsx
@@ -1,18 +1,13 @@
 import { LanguageButton } from '@components/atoms/LanguageButton';
-import Link from 'next/link';
-import { useTranslations } from 'next-intl';
+import LinkLog from '@components/atoms/LinkLogIn';
+import LinkPricing from '@components/atoms/LinkPricing';
 
 export default function NavLinks() {
-  const t = useTranslations('Landing');
   return (
     <nav className="hidden items-center gap-6 md:flex">
       <LanguageButton />
-      <Link href="#" className="text-gray-700 hover:text-gray-900">
-        {t('pricing')}
-      </Link>
-      <Link href="#" className="text-gray-700 hover:text-gray-900">
-        {t('login')}
-      </Link>
+      <LinkLog />
+      <LinkPricing />
     </nav>
   );
 }
