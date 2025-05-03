6521b080a521526c20c30047b4bdc590906e3b9a
hadelacruz <humbertoalexanderdelacruz@gmail.com>
2025-05-02 23:01:29 -0600

    feat: :lipstick: add organism start

--- easystore-app/app/[locale]/components/organisms/landing/Portrait.tsx
+++ easystore-app/app/[locale]/components/organisms/landing/Portrait.tsx
@@ -1,43 +1,36 @@
 'use client';
-import { ButtonBase } from '@components/atoms/landing/ButtonBase';
+import ButtonBase from '@components/atoms/landing/ButtonBase';
+import ButtonViewPlans from '@components/atoms/landing/ButtonViewPlans';
 import Link from 'next/link';
 import { useTranslations } from 'next-intl';
 
-export const Portrait = () => {
+export default function Portrait() {
   const t = useTranslations('Landing');
-
   return (
     <section
       className="mx-0 my-3 min-h-[770px] rounded-xl bg-cover bg-center text-white sm:mx-7 sm:min-h-[700px]"
       style={{
-        backgroundImage: `url('/portrait_image.svg')`,
+        backgroundImage: `url('/portrait_image.webp')`,
       }}
     >
       <div className="max-w-screen-xl py-20 sm:ml-13">
         <div className="ml-0 max-w-85 pl-5 sm:max-w-xl">
           <h1 className="mb-13 text-5xl font-extrabold sm:text-6xl">
-            Grow Your Brand Effortlessly with EasyStore
+            {t('title')}
           </h1>
           <p className="font-regular text-text mb-40 text-xl sm:text-2xl">
-            Build a powerful online store that adapts to any business – no
-            coding or developer needed!
+            {t('slogan')}
           </p>
           <div className="flex gap-4">
             <Link href="/signup">
-              <ButtonBase
-                label={t('buttonStartFree')}
-                className="border-primary border-3 px-5 py-5 text-lg sm:px-8 sm:py-6 sm:text-xl"
-              />
+              <ButtonBase />
             </Link>
             <Link href="/plans">
-              <ButtonBase
-                label={t('buttonStartFree')}
-                className="border-3 border-white bg-transparent px-5 py-5 text-lg sm:px-8 sm:py-6 sm:text-xl"
-              />
+              <ButtonViewPlans />
             </Link>
           </div>
         </div>
       </div>
     </section>
   );
-};
+}
