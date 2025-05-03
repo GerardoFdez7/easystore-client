37d84d30f447e5306540e54a9ada4089c3310aef
hadelacruz <humbertoalexanderdelacruz@gmail.com>
2025-05-02 23:01:29 -0600

    feat: :lipstick: add organism start

---
+++ easystore-app/app/[locale]/components/atoms/landing/ButtonViewPlans.tsx
@@ -0,0 +1,12 @@
+import { useTranslations } from 'next-intl';
+import { Button } from '@components/atoms/shared/ButtonCn';
+
+export default function ButtonViewPlans() {
+  const t = useTranslations('Landing');
+
+  return (
+    <Button className=": flex h-[70px] items-center justify-center rounded-full border-3 border-white bg-transparent text-2xl font-extrabold text-white max-[580px]:h-[12vw] max-[580px]:w-[35vw] max-[580px]:min-w-[33vw] max-[580px]:text-[4vw]">
+      {t('buttonViewPlans')}
+    </Button>
+  );
+}
--- easystore-app/app/[locale]/components/templates/Landing.tsx
+++ easystore-app/app/[locale]/components/templates/Landing.tsx
@@ -1,10 +1,13 @@
 import HeaderLanding from '@components/organisms/landing/HeaderLanding';
-import { Portrait } from '@components/organisms/landing/Portrait';
+import Portrait from '@components/organisms/landing/Portrait';
+import Start from '@components/organisms/landing/Start';
 
 export default function LandingPage() {
   return (
     <div className="bg-background min-h-screen">
       <HeaderLanding />
+      <Portrait />
+      <Start />
     </div>
   );
 }
