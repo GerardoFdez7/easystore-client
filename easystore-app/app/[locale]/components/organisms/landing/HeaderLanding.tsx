a58f7843c542c52ea3a3bc3de20adc294d04d58f
hadelacruz <humbertoalexanderdelacruz@gmail.com>
2025-05-03 23:37:57 -0600

    feat: :lipstick: add organism footer

--- easystore-app/app/[locale]/components/organisms/landing/HeaderLanding.tsx
+++ easystore-app/app/[locale]/components/organisms/landing/HeaderLanding.tsx
@@ -5,7 +5,7 @@ import NaviLinks from '@components/molecules/landing/NaviLinks';
 
 export default function HeaderLanding() {
   return (
-    <header className="mx-3 mt-4 flex items-center justify-between sm:mx-10">
+    <header className="bg-background fixed top-0 right-0 left-0 z-50 mt-0 flex h-20 items-center justify-between px-3 pt-4 pb-4 shadow-md sm:h-25 sm:px-10">
       <Logo />
       <div className="flex items-center gap-2">
         <NaviLinks />
