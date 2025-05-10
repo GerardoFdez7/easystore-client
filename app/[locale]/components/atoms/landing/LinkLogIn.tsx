beddea0abc4af9333169f4121a1043e940f07a56
diegoanro22 <82407480+diegoanro22@users.noreply.github.com>
2025-05-10 16:42:49 -0600

    perf: :construction: add link to the login

--- app/[locale]/components/atoms/landing/LinkLogIn.tsx
+++ app/[locale]/components/atoms/landing/LinkLogIn.tsx
@@ -4,7 +4,10 @@ import Link from 'next/link';
 export default function LinkLog() {
   const t = useTranslations('Landing');
   return (
-    <Link href="/login" className="text-title text-2xl font-medium">
+    <Link
+      href="/login/"
+      className="text-text text-2xl font-medium hover:text-gray-900"
+    >
       {t('login')}
     </Link>
   );
