bb5454380873548f428e1820959c014210211360
diegoanro22 <82407480+diegoanro22@users.noreply.github.com>
2025-05-10 05:08:26 -0600

    feat: :construction: add structure of the login page
    create folders and files for the login page

---
+++ easystore-app/app/[locale]/components/templates/Login.tsx
@@ -0,0 +1,9 @@
+import Footer from '@organisms/shared/Footer';
+
+export default function LoginPage() {
+  return (
+    <div className="bg-background min-h-screen">
+      <Footer />
+    </div>
+  );
+}
