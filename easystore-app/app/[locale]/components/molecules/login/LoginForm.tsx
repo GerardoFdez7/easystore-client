4eee3e89c71ac7659635f2ecae739c40dabf47d4
diegoanro22 <82407480+diegoanro22@users.noreply.github.com>
2025-05-11 00:34:21 -0600

    feat: :sparkles: add links in the login form

--- easystore-app/app/[locale]/components/molecules/login/LoginForm.tsx
+++ easystore-app/app/[locale]/components/molecules/login/LoginForm.tsx
@@ -3,12 +3,16 @@
 import { FormField } from '../shared/FormField';
 import { Form } from '@components/atoms/shared/Form';
 import ButtonLogin from '@components/atoms/login/ButtonLogin';
+import React from 'react';
+import { useTranslations } from 'next-intl';
+import LinkText from '@components/atoms/shared/LinkText';
 
 export interface LoginFormProps {
   onSubmit: (data: { email: string; password: string }) => void;
 }
 
 export const LoginForm: React.FC<LoginFormProps> = ({ onSubmit }) => {
+  const t = useTranslations('Login');
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
@@ -19,24 +23,39 @@ export const LoginForm: React.FC<LoginFormProps> = ({ onSubmit }) => {
   };
 
   return (
-    <Form.Root onSubmit={handleSubmit} className="w-full max-w-sm space-y-6">
+    <Form.Root onSubmit={handleSubmit} className="w-full max-w-sm space-y-4">
       <FormField
         name="email"
-        label="Correo electrónico"
+        label={t('email')}
         type="email"
         placeholder="tucorreo@ejemplo.com"
       />
 
       <FormField
         name="password"
-        label="Contraseña"
+        label={t('password')}
         type="password"
         placeholder="••••••••"
       />
 
+      <div className="flex flex-col items-center">
+        <p className="text-sm text-gray-600">
+          {t('MessageAccount')}{' '}
+          <LinkText href="/register" className="text-secondary">
+            {t('Register')}
+          </LinkText>
+        </p>
+      </div>
+
       <Form.Submit asChild>
         <ButtonLogin />
       </Form.Submit>
+
+      <div className="flex flex-col items-center">
+        <LinkText href="/register" className="">
+          {t('ChangePassword')}
+        </LinkText>
+      </div>
     </Form.Root>
   );
 };
