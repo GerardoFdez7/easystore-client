fb1d6844462be6be690373597007732875d4cf4f
diegoanro22 <82407480+diegoanro22@users.noreply.github.com>
2025-05-10 19:52:15 -0600

    feat: :sparkles: implement form login

--- app/[locale]/components/templates/Login.tsx
+++ app/[locale]/components/templates/Login.tsx
@@ -1,10 +1,12 @@
 import Footer from '@organisms/shared/Footer';
 import HeaderLogin from '@components/organisms/login/HeaderLogin';
+import MainLogin from '@components/organisms/login/MainLogin';
 
 export default function LoginPage() {
   return (
     <div className="bg-background min-h-screen">
       <HeaderLogin />
+      <MainLogin />
       <Footer />
     </div>
   );
