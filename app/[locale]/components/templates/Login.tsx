4a32b37df9b00513ed03f05352a3d5d51c808cb3
diegoanro22 <82407480+diegoanro22@users.noreply.github.com>
2025-05-10 18:48:14 -0600

    feat: :sparkles: implement header login
    create header login with the languagebutton

--- app/[locale]/components/templates/Login.tsx
+++ app/[locale]/components/templates/Login.tsx
@@ -1,8 +1,10 @@
 import Footer from '@organisms/shared/Footer';
+import HeaderLogin from '@components/organisms/login/HeaderLogin';
 
 export default function LoginPage() {
   return (
     <div className="bg-background min-h-screen">
+      <HeaderLogin />
       <Footer />
     </div>
   );
