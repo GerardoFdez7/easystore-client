95cdd526db296f468d0233919a25fe0a26af51ca
diegoanro22 <82407480+diegoanro22@users.noreply.github.com>
2025-05-11 03:54:05 -0600

    feat: :sparkles: implement registerform in register page

--- app/[locale]/components/templates/Register.tsx
+++ app/[locale]/components/templates/Register.tsx
@@ -1,10 +1,12 @@
 import Footer from '@organisms/shared/Footer';
 import HeaderRegister from '@components/organisms/register/HeaderRegister';
+import MainRegister from '@components/organisms/register/MainRegister';
 
 export default function RegisterPage() {
   return (
     <div className="bg-background min-h-screen">
       <HeaderRegister />
+      <MainRegister />
       <Footer />
     </div>
   );
