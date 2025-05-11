fa38ae268dd9cfd967481f0439703fd39d644ee8
diegoanro22 <82407480+diegoanro22@users.noreply.github.com>
2025-05-11 03:00:21 -0600

    feat: :construction: create structure of the register page

---
+++ easystore-app/app/[locale]/components/templates/Register.tsx
@@ -0,0 +1,11 @@
+import Footer from '@organisms/shared/Footer';
+import HeaderLogin from '@components/organisms/login/HeaderLogin';
+
+export default function RegisterPage() {
+  return (
+    <div className="bg-background min-h-screen">
+      <HeaderLogin />
+      <Footer />
+    </div>
+  );
+}
