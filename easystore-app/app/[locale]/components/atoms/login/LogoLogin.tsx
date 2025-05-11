be72bfde298b3f98bb9f2285e23b0361aa5ead66
diegoanro22 <82407480+diegoanro22@users.noreply.github.com>
2025-05-10 21:19:09 -0600

    feat: :sparkles: create header of the login form

--- easystore-app/app/[locale]/components/atoms/login/LogoLogin.tsx
+++ easystore-app/app/[locale]/components/atoms/login/LogoLogin.tsx
@@ -5,13 +5,13 @@ import Logo from '@components/atoms/shared/Logo';
 
 export default function LogoLogin() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center p-4">
+    <div className="flex flex-col items-center justify-center p-4">
       <Logo
         src="/logo.webp"
         alt="EasyStore Logo"
-        label="EasyStore"
-        width={72}
-        height={72}
+        label=""
+        width={85}
+        height={85}
         className="mb-8"
         labelClassName="text-2xl font-bold text-gray-900"
       />
