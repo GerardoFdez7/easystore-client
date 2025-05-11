673726a7499f883f452faca80690ac1c0245b897
diegoanro22 <82407480+diegoanro22@users.noreply.github.com>
2025-05-10 19:05:15 -0600

    feat: :sparkles: create Form Atom

---
+++ app/[locale]/components/atoms/shared/Form.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import * as FormPrimitive from '@radix-ui/react-form';
+
+export const Form = {
+  Root: FormPrimitive.Root,
+  Field: FormPrimitive.Field,
+  Control: FormPrimitive.Control,
+  Label: FormPrimitive.Label,
+  Message: FormPrimitive.Message,
+  Submit: FormPrimitive.Submit,
+};
