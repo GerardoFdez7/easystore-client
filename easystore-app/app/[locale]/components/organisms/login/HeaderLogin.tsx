7d403984eb52196cbe69ae252f22297ebe0b44c8
diegoanro22 <82407480+diegoanro22@users.noreply.github.com>
2025-05-11 02:38:40 -0600

    feat: :sparkles: add socialbuttons in login page

--- easystore-app/app/[locale]/components/organisms/login/HeaderLogin.tsx
+++ easystore-app/app/[locale]/components/organisms/login/HeaderLogin.tsx
@@ -2,7 +2,7 @@ import NaviLinks from '@components/molecules/login/NaviLinks';
 
 export default function HeaderLogin() {
   return (
-    <header className="bg-background fixed top-0 right-0 left-0 z-50 mt-0 flex h-20 items-center justify-end px-3 pt-4 pb-4 sm:h-25 sm:px-10">
+    <header className="bg-background sticky top-0 right-0 left-0 z-50 mt-0 flex h-12 items-center justify-end px-3 pt-2 pb-2 sm:h-16 sm:px-10">
       <div className="flex items-center gap-2">
         <NaviLinks />
       </div>
