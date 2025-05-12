f40c4a5aee007bd3999abd192b686fa6c7115a2f
diegoanro22 <82407480+diegoanro22@users.noreply.github.com>
2025-05-11 20:14:39 -0600

    feat: :sparkles: create Card Atom

---
+++ easystore-app/app/[locale]/components/atoms/register/ButtonConfirmRegister.tsx
@@ -0,0 +1,12 @@
+import { useTranslations } from 'next-intl';
+import { Button } from '@atoms/shared/ButtonCn';
+
+export default function ButtonLogin() {
+  const t = useTranslations('Register');
+
+  return (
+    <Button className="bg-primary text-text flex h-[50px] w-full items-center justify-center rounded-full font-bold text-white max-[580px]:h-[12vw] max-[580px]:w-[35vw] max-[580px]:min-w-[33vw] max-[580px]:text-[4vw]">
+      {t('buttonRegisterConfirm')}
+    </Button>
+  );
+}
---
+++ easystore-app/app/[locale]/components/templates/ConfirmRegister.tsx
@@ -0,0 +1,13 @@
+import HeaderRegister from '@components/organisms/register/HeaderRegister';
+
+import Footer from '@organisms/shared/Footer';
+
+export default function ConfirmRegister() {
+  return (
+    <div className="bg-background min-h-screen">
+      <HeaderRegister />
+
+      <Footer />
+    </div>
+  );
+}
