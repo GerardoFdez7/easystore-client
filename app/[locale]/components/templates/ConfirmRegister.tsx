49fad888f81646d10e3fdafe6cd10e6cf5ab0fb6
diegoanro22 <82407480+diegoanro22@users.noreply.github.com>
2025-05-11 23:32:35 -0600

    feat: :sparkles: add form of the confirm-register-page
    complete the view of the register-confirm page, add the form that contains the input of the bussiness name and the plans

--- app/[locale]/components/templates/ConfirmRegister.tsx
+++ app/[locale]/components/templates/ConfirmRegister.tsx
@@ -1,12 +1,12 @@
 import HeaderRegister from '@components/organisms/register/HeaderRegister';
-
+import MainConfirmRegister from '@components/organisms/register/MainConfirmRegister';
 import Footer from '@organisms/shared/Footer';
 
 export default function ConfirmRegister() {
   return (
     <div className="bg-background min-h-screen">
       <HeaderRegister />
-
+      <MainConfirmRegister />
       <Footer />
     </div>
   );
