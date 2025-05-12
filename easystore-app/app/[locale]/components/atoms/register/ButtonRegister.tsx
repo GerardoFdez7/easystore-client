dad61821e14959066c48ccef79c146d2b31d829e
diegoanro22 <82407480+diegoanro22@users.noreply.github.com>
2025-05-11 19:24:01 -0600

    feat: :sparkles: create confirmpage register

--- easystore-app/app/[locale]/components/atoms/register/ButtonRegister.tsx
+++ easystore-app/app/[locale]/components/atoms/register/ButtonRegister.tsx
@@ -6,7 +6,7 @@ export default function ButtonLogin() {
 
   return (
     <Button className="bg-primary text-text flex h-[50px] w-full items-center justify-center rounded-full font-bold text-white max-[580px]:h-[12vw] max-[580px]:w-[35vw] max-[580px]:min-w-[33vw] max-[580px]:text-[4vw]">
-      {t('buttonRegister')}
+      {t('buttonRegisterContinue')}
     </Button>
   );
 }
