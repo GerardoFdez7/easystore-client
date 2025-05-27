c516cedef736190081f6805a73d6b4c074210f73
diegoanro22 <82407480+diegoanro22@users.noreply.github.com>
2025-05-27 13:24:17 -0600

    fix: :adhesive_bandage: implement the mobile view in the register page

--- easystore-app/app/[locale]/components/atoms/login/ButtonLogin.tsx
+++ easystore-app/app/[locale]/components/atoms/login/ButtonLogin.tsx
@@ -5,7 +5,7 @@ export default function ButtonLogin() {
   const t = useTranslations('Login');
 
   return (
-    <Button className="bg-primary flex h-10 w-full items-center justify-center rounded-full text-base font-bold text-white sm:h-14 sm:text-lg md:h-14 md:text-xl">
+    <Button className="bg-primary text-text flex h-[50px] w-full items-center justify-center rounded-full font-bold text-white max-[580px]:h-[10vw] max-[580px]:w-[full] max-[580px]:min-w-[33vw] max-[580px]:text-[4vw]">
       {t('buttonLogin')}
     </Button>
   );
