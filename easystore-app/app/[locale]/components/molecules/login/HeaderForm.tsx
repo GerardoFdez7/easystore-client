4137c938cecc5a62c77d8ffd3210654ee92f27fc
diegoanro22 <82407480+diegoanro22@users.noreply.github.com>
2025-05-26 18:27:48 -0600

    fix: :adhesive_bandage: make the mobile view and make it responsive

--- easystore-app/app/[locale]/components/molecules/login/HeaderForm.tsx
+++ easystore-app/app/[locale]/components/molecules/login/HeaderForm.tsx
@@ -6,14 +6,14 @@ import { useTranslations } from 'next-intl';
 export default function HeaderForm() {
   const t = useTranslations('Login');
   return (
-    <section className="flex flex-col items-center justify-between px-4 sm:flex-row sm:px-8 md:px-16 lg:px-32">
+    <section className="flex flex-col items-center justify-between px-15 sm:flex-row sm:px-8 md:px-16 lg:px-32">
       <div className="flex items-center space-x-4">
         <LogoLogin />
         <div className="text-center sm:text-left">
           <h1 className="text-title text-2xl font-extrabold sm:text-3xl md:text-4xl">
             {t('welcomeBack')}
           </h1>
-          <p className="text-text text-sm sm:text-base md:text-lg">
+          <p className="text-text text-primary text-sm sm:text-base md:text-lg">
             {t('loginMessage')}
           </p>
         </div>
