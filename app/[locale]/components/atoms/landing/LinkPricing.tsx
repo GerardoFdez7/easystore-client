b0841c47c61e1491bfd9b676b408cd56719b5152
hadelacruz <humbertoalexanderdelacruz@gmail.com>
2025-06-07 16:38:15 -0600

    fix: :bug: update href of pricing buttons and view plans

--- app/[locale]/components/atoms/landing/LinkPricing.tsx
+++ app/[locale]/components/atoms/landing/LinkPricing.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function LinkPricing() {
   const t = useTranslations('Landing');
   return (
-    <Link href="#" className="text-title text-2xl font-medium">
+    <Link href="#plans" className="text-title text-2xl font-medium">
       {t('pricing')}
     </Link>
   );
