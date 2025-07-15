fa5c16b2f7a615a4d985d24313a9d513e43286d6
diegoanro22 <82407480+diegoanro22@users.noreply.github.com>
2025-07-15 01:19:41 -0600

    feat: :sparkles: add PrivacyContent organism
    update ContentSection, TableOfContents and PrivacyTemplate components with design changes and add translations for Privacy page

--- app/[locale]/components/templates/Privacy.tsx
+++ app/[locale]/components/templates/Privacy.tsx
@@ -1,10 +1,12 @@
 import Footer from '@organisms/shared/Footer';
 import HeaderLanding from '@organisms/landing/HeaderLanding';
+import { PrivacyContent } from '@organisms/privacy/PrivacyContent';
 
 export default function PrivacyTemplate() {
   return (
     <div className="bg-background flex min-h-screen flex-col">
       <HeaderLanding />
+      <PrivacyContent />
       <Footer />
     </div>
   );
