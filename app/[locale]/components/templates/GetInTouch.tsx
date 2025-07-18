65d10d3e91c586d31f3de5c33e17506605df08dd
diegoanro22 <82407480+diegoanro22@users.noreply.github.com>
2025-07-17 20:28:15 -0600

    feat: :sparkles: add molecules and organisms for get in touch page
    add MainTouch, FeatureList and ContactField component

--- app/[locale]/components/templates/GetInTouch.tsx
+++ app/[locale]/components/templates/GetInTouch.tsx
@@ -1,10 +1,12 @@
 import Footer from '@organisms/shared/Footer';
 import HeaderTouch from '@organisms/get-in-touch/HeaderTouch';
+import MainTouch from '@organisms/get-in-touch/MainTouch';
 
 export default function GetInTouchTemplate() {
   return (
     <div className="bg-background flex min-h-screen flex-col">
       <HeaderTouch />
+      <MainTouch />
       <Footer />
     </div>
   );
