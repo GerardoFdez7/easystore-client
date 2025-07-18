ee8fb36498fc4aaec3e4b852f396e8865cfec1ab
diegoanro22 <82407480+diegoanro22@users.noreply.github.com>
2025-07-17 18:23:30 -0600

    feat: :sparkles: add get in touch page with header
    create header organisms and template of the get in touch page

---
+++ app/[locale]/components/organisms/get-in-touch/HeaderTouch.tsx
@@ -0,0 +1,15 @@
+import Logo from '@atoms/landing/Logo';
+import MobileMenu from '@molecules/landing/MobileMenu';
+import { LanguageButton } from '@atoms/shared/ButtonLanguage';
+
+export default function HeaderTouch() {
+  return (
+    <header className="bg-background fixed top-0 right-0 left-0 z-50 flex h-20 items-center justify-between px-3 pt-4 pb-4 sm:h-25 sm:px-10">
+      <Logo />
+      <div className="flex items-center gap-2">
+        <LanguageButton />
+        <MobileMenu />
+      </div>
+    </header>
+  );
+}
---
+++ app/[locale]/touch/page.tsx
@@ -0,0 +1,5 @@
+import GetInTouchTemplate from '@templates/GetInTouch';
+
+export default function GetInTouch() {
+  return <GetInTouchTemplate />;
+}
