2eb39b95df04755056a0ecde7e342601e1726c70
diegoanro22 <82407480+diegoanro22@users.noreply.github.com>
2025-07-24 14:05:24 -0600

    feat: add edit functionality to profile components
    add edit functionality to ProfileLogo, DescriptionEditor, and EditableField components, including buttons to toggle edit mode and input fields to edit profile information.

--- app/[locale]/components/templates/Profile.tsx
+++ app/[locale]/components/templates/Profile.tsx
@@ -1,13 +1,11 @@
-import Footer from '@organisms/shared/Footer';
-import HeaderLogin from '@organisms/login/HeaderLogin';
-import MainLogin from '@organisms/login/MainLogin';
+import MainProfile from '@organisms/profile/MainProfile';
+import HeaderProfile from '@organisms/profile/HeaderProfile';
 
 export default function ProfileTemplate() {
   return (
     <div className="bg-background flex min-h-screen flex-col">
-      <HeaderLogin />
-      <MainLogin />
-      <Footer />
+      <HeaderProfile />
+      <MainProfile />
     </div>
   );
 }
