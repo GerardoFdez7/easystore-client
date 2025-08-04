b4a8b14854761a28a85276469b12bc3c59a1725f
hadelacruz <humbertoalexanderdelacruz@gmail.com>
2025-08-03 19:32:57 -0600

    style: :lipstick: enhance ProductsToolbar responsive layout and SearchBar flexibility

--- app/[locale]/components/atoms/products/SearchBar.tsx
+++ app/[locale]/components/atoms/products/SearchBar.tsx
@@ -7,13 +7,9 @@ interface SearchBarProps {
   onChange?: (value: string) => void;
 }
 
-export function SearchBar({
-  placeholder = 'Search products',
-  value,
-  onChange,
-}: SearchBarProps) {
+export function SearchBar({ placeholder, value, onChange }: SearchBarProps) {
   return (
-    <div className="relative max-w-md flex-1">
+    <div className="relative max-w-[550px] min-w-[200px] flex-1 sm:max-w-2xl lg:max-w-3xl 2xl:max-w-4xl">
       <Search className="text-foreground absolute top-1/2 left-3 h-4 w-4 -translate-y-1/2 transform" />
       <Input
         placeholder={placeholder}
