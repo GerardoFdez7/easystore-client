d0b6790e2817f2dbb1556e376046b5755a1bc11d
diegoanro22 <82407480+diegoanro22@users.noreply.github.com>
2025-08-07 20:39:24 -0600

    feat: :sparkles: create components for the category page

---
+++ app/[locale]/components/atoms/category/WelcomeCategory.tsx
@@ -0,0 +1,13 @@
+import { useTranslations } from 'next-intl';
+
+export default function WelcomeCategory() {
+  const t = useTranslations('Category');
+
+  return (
+    <div className="mx-5 mb-8">
+      <h1 className="text-title mb-2 text-2xl font-bold sm:text-4xl">
+        {t('welcomeCategory')}
+      </h1>
+    </div>
+  );
+}
