62ba28ff670167246c1230392144e3519e55a840
diegoanro22 <82407480+diegoanro22@users.noreply.github.com>
2025-08-11 09:54:19 -0600

    feat: :sparkles: replace Combobox with Select for annual revenue input; enhance UI consistency

--- app/[locale]/components/molecules/get-in-touch/ContactFields.tsx
+++ app/[locale]/components/molecules/get-in-touch/ContactFields.tsx
@@ -11,6 +11,13 @@ import {
   FormMessage,
 } from '@shadcn/ui/form';
 import InputCn from '@atoms/shared/OutsideInput';
+import {
+  Select,
+  SelectTrigger,
+  SelectValue,
+  SelectContent,
+  SelectItem,
+} from '@shadcn/ui/select';
 import { RadioGroup, RadioGroupItem } from '@shadcn/ui/radio-group';
 import { Combobox } from '@shadcn/ui/combobox';
 import { COUNTRIES } from '@lib/consts/countries';
@@ -155,20 +162,17 @@ export const ContactFields: React.FC = () => {
             <FormItem>
               <FormLabel>{t('annualRevenue')}</FormLabel>
               <FormControl>
-                <div className="relative">
-                  <Select value={field.value} onValueChange={field.onChange}>
-                    <SelectTrigger className="border-primary w-full [&>svg]:hidden">
-                      <SelectValue placeholder={t('selectAnnualRevenue')} />
-                    </SelectTrigger>
-                    <SelectContent>
-                      <SelectItem value="0-100k">$0 – $100k</SelectItem>
-                      <SelectItem value="100k-500k">$100k – $500k</SelectItem>
-                      <SelectItem value="500k-1m">$500k – $1M</SelectItem>
-                      <SelectItem value="1m+">$1M+</SelectItem>
-                    </SelectContent>
-                  </Select>
-                  <CircleChevronDown className="text-primary pointer-events-none absolute top-1/2 right-3 z-10 h-5 w-5 -translate-y-1/2" />
-                </div>
+                <Select value={field.value} onValueChange={field.onChange}>
+                  <SelectTrigger className="h-12 w-full">
+                    <SelectValue placeholder={t('annualRevenue')} />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="0-100k">$0 – $100k</SelectItem>
+                    <SelectItem value="100k-500k">$100k – $500k</SelectItem>
+                    <SelectItem value="500k-1m">$500k – $1M</SelectItem>
+                    <SelectItem value="1m+">$1M+</SelectItem>
+                  </SelectContent>
+                </Select>
               </FormControl>
               <FormMessage />
             </FormItem>
