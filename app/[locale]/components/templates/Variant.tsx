d1b2ae01422db14f60b5246a36dc4742ce9e4548
diegoanro22 <82407480+diegoanro22@users.noreply.github.com>
2025-08-17 19:26:59 -0600

    refactor: refactor HeaderDashboard component to be shared

--- app/[locale]/components/templates/Variant.tsx
+++ app/[locale]/components/templates/Variant.tsx
@@ -1,10 +1,10 @@
-import HeaderVariant from '@organisms/variant/HeaderVariant';
 import MainVariant from '@organisms/variant/MainVariant';
+import HeaderDashboard from '@organisms/shared/HeaderDashboard';
 
 export default function VariantTemplate() {
   return (
     <div className="bg-background flex min-h-screen flex-col">
-      <HeaderVariant />
+      <HeaderDashboard />
       <MainVariant />
     </div>
   );
