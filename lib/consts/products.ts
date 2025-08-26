8d4aecdefd47cd757c1418111accb13230564411
hadelacruz <humbertoalexanderdelacruz@gmail.com>
2025-08-25 22:36:39 -0600

    style: :lipstick: update the product grid style

--- lib/consts/products.ts
+++ lib/consts/products.ts
@@ -4,25 +4,68 @@ export interface Product {
   name: string;
   brand?: string | null;
   cover: string;
-  media?: Array<{
-    url: string;
-    mediaType: 'IMAGE' | 'VIDEO';
-    position: number;
-  }> | null;
+  longDescription?: string | null;
+  manufacturer?: string | null;
   isArchived: boolean;
+  productType: string;
+  shortDescription: string;
+  tags?: Array<string> | null;
+  categories?: Array<{
+    __typename?: 'ProductCategory';
+    categoryId: string;
+  }> | null;
+  media?: Array<{
+    __typename?: 'Media';
+    mediaType: string;
+    position: number;
+    productId?: string | null;
+    url: string;
+    variantId?: string | null;
+  }> | null;
+  sustainabilities?: Array<{
+    __typename?: 'Sustainability';
+    certification: string;
+    recycledPercentage: number;
+  }> | null;
   variants?: Array<{
-    sku?: string | null;
-    price: number;
+    __typename?: 'Variant';
     barcode?: string | null;
-    condition: string;
     ean?: string | null;
     isbn?: string | null;
-    personalizationOptions?: string[] | null;
+    personalizationOptions?: Array<string> | null;
+    price: number;
+    sku?: string | null;
     upc?: string | null;
     variantCover?: string | null;
     weight?: number | null;
-  }> | null;
-  categories?: Array<{
-    categoryId: string;
+    attributes: Array<{
+      __typename?: 'Attribute';
+      key: string;
+      value: string;
+    }>;
+    dimension?: {
+      __typename?: 'Dimension';
+      height: number;
+      length: number;
+      width: number;
+    } | null;
+    installmentPayments?: Array<{
+      __typename?: 'Installment';
+      interestRate: number;
+      months: number;
+    }> | null;
+    variantMedia?: Array<{
+      __typename?: 'Media';
+      position: number;
+      productId?: string | null;
+      url: string;
+      variantId?: string | null;
+    }> | null;
+    warranties?: Array<{
+      __typename?: 'Warranty';
+      coverage: string;
+      instructions: string;
+      months: number;
+    }> | null;
   }> | null;
 }
