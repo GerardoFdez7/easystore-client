8f60c78d6f6a5af0e5abd6c9aa43eb4f8f562039
jruiz002 <josegrg04@gmail.com>
2025-09-19 18:51:19 -0600

    feat: add stock movement page and components

---
+++ app/[locale]/components/templates/StockMovement.tsx
@@ -0,0 +1,14 @@
+import HeaderDashboard from '@organisms/shared/HeaderDashboard';
+import SidebarLayout from '@organisms/shared/SidebarLayout';
+import MainStockMovement from '@organisms/stock-movement/MainStockMovement';
+
+export default function StockMovementTemplate() {
+  return (
+    <>
+      <HeaderDashboard />
+      <SidebarLayout title="Stock Movement">
+        <MainStockMovement />
+      </SidebarLayout>
+    </>
+  );
+}
