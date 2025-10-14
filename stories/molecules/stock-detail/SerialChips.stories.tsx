2cf0f71c16fd6d9872d7a23ee7f127d3e8851928
diegoanro22 <82407480+diegoanro22@users.noreply.github.com>
2025-10-13 19:20:02 -0600

    feat(storybook): :memo: add story files for stock pages
    add story for
    - InventoryTable
    - CalendarPicker
    - SerialChips
    - UpdateReasonDialog
    - MainStockDetail
    - MainStockMovement
    - StockDetail
    - StockMovement

---
+++ stories/molecules/stock-detail/SerialChips.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/nextjs';
+import SerialChips from '@molecules/stock-detail/SerialChips';
+
+const meta: Meta<typeof SerialChips> = {
+  title: 'Molecules/StockDetail/SerialChips',
+  component: SerialChips,
+  tags: ['autodocs'],
+};
+export default meta;
+
+type Story = StoryObj<typeof SerialChips>;
+
+export const Empty: Story = {
+  args: {
+    serials: [],
+  },
+};
+
+export const WithSerials: Story = {
+  args: {
+    serials: ['SN-001', 'SN-002', 'SN-ABC-9999'],
+  },
+};
---
+++ stories/template/StockMovement.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/nextjs';
+import StockMovementTemplate from '@templates/StockMovement';
+
+const meta: Meta<typeof StockMovementTemplate> = {
+  title: 'Templates/StockMovement',
+  component: StockMovementTemplate,
+  parameters: {
+    layout: 'fullscreen',
+    nextjs: {
+      appDirectory: true,
+    },
+  },
+  tags: ['autodocs'],
+};
+export default meta;
+
+type Story = StoryObj<typeof StockMovementTemplate>;
+
+export const Default: Story = {};
